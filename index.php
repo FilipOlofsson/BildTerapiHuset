45dc3ce4b0295114508ad176df4f0c5596a3ec4a
FilipOlofsson <te15.filip.olofsson@umea.nti.se>
2018-03-12 14:42:02 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
 
 ?>
-
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="main-page-container">
+            <div class="main-page-cover">
+                <nav id="navbar">
+                    <a class="navbar-link" href="#">UPPDRAG</a>
+                    <a class="navbar-link" href="#">OSS</a>
+                    <a class="navbar-link" href="#">LÄNKAR</a>
+                    <a class="navbar-link" href="#">HITTA HIT</a>
+                </nav>
+                <img class="logo-img" src="img/logo.svg">
+            </div>
+        </div>
+    </body>
+</html>
