00e8fcac94ca63ba565406c67442587755f2b3ce
FilipOlofsson <te15.filip.olofsson@umea.nti.se>
2018-03-12 19:44:00 +0100

    Mobile support added

--- index.php
+++ index.php
@@ -1,28 +1,36 @@
-<?php
-
-?>
+<!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" href="css/style.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
         rel="stylesheet">
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="js/script.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bildterapihuset</title>
     </head>
-    <body>
+    <body id="body">
         <div class="all-pages">
             <div class="main-page-container">
                 <div class="main-page-cover">
+                    <div class="hamburger-toggle" id="toggle-hamburger" onclick="toggleHamburger()"></div>
+                    <div class="hamburger-menu" id="hamburger-menu">
+                        <div class="mobile-navbar-header">
+                            <div class="close-hamburger" onclick="toggleHamburger()"></div>
+                        </div>
+                        <div class="mobile-navbar-link" onclick="window.location = '#second-page'; toggleHamburger();">UPPDRAG</div>
+                        <div class="mobile-navbar-link" onclick="window.location = '#third-page'; toggleHamburger();">OSS</div>
+                        <div class="mobile-navbar-link" onclick="window.location = '#fourth-page'; toggleHamburger();">HITTA HIT</div>
+                    </div>
                     <nav id="navbar">
                         <a class="navbar-link" href="#">UPPDRAG</a>
                         <a class="navbar-link" href="#">OSS</a>
-                        <a class="navbar-link" href="#">LÄNKAR</a>
                         <a class="navbar-link" href="#">HITTA HIT</a>
                     </nav>
-                    <img class="logo-img" src="img/logo.svg">
+                    <img alt="Logo" class="logo-img" id="logo-img" src="img/logo.svg">
                 </div>
             </div>
-            <div class="second-page">
+            <div id="second-page" class="second-page">
                 <div class="second-page-box">
                     <div class="box-header">
                         <i class="material-icons box-icon">person</i>
@@ -60,54 +68,55 @@
                     </div>
                 </div>
             </div>
-            <div class="third-page">
+            <div id="third-page" class="third-page">
                 <div class="row-container">
                     <div class="row-box">
-                        <img class="terapi-img" src="img/anna.jpg">
+                        <img alt="Anna Karin Karlsson" class="terapi-img" src="img/anna.jpg">
                         <h3 class="row-box-title">Anna Karin Karlsson</h3>
                     </div>
                     <div class="row-box">
-                        <img class="terapi-img" src="img/birgitta.jpg">
+                        <img alt="Birgitta Englund" class="terapi-img" src="img/birgitta.jpg">
                         <h3 class="row-box-title">Birgitta Englund</h3>
                     </div>
                     <div class="row-box">
-                        <img class="terapi-img" src="img/carin.jpg">
+                        <img alt="Carin Salander" class="terapi-img" src="img/carin.jpg">
                         <h3 class="row-box-title">Carin Salander</h3>
                     </div>
                     <div class="row-box">
-                        <img class="terapi-img" src="img/gunilla-ny.jpg">
+                        <img alt="Gunilla Berglund" class="terapi-img" src="img/gunilla-ny.jpg">
                         <h3 class="row-box-title">Gunilla Berglund</h3>
                     </div>
                 </div>
                 <div class="row-container">
                     <div class="row-box">
-                        <img class="terapi-img" src="img/karinet.png">
+                        <img alt="Karin Egberg Thyme" class="terapi-img" src="img/karinet.png">
                         <h3 class="row-box-title">Karin Egberg Thyme</h3>
                     </div>
                     <div class="row-box">
-                        <img class="terapi-img" src="img/placeholder.png">
+                        <img alt="Katarina Sihlén" class="terapi-img" src="img/placeholder.png">
                         <h3 class="row-box-title">Katarina Sihlén</h3>
                     </div>
                     <div class="row-box">
-                        <img class="terapi-img" src="img/ninne.jpg">
+                        <img alt="Ninne Lindgren" class="terapi-img" src="img/ninne.jpg">
                         <h3 class="row-box-title">Ninne Lindgren</h3>
                     </div>
                     <div class="row-box">
-                        <img class="terapi-img" src="img/ulla.jpg">
+                        <img alt="Ulla Kiivuori" class="terapi-img" src="img/ulla.jpg">
                         <h3 class="row-box-title">Ulla Kiivuori</h3>
                     </div>
                 </div>
             </div>
-            <div class="fourth-page">
+            <div id="fourth-page" class="fourth-page">
                 <div class="left-side">
                     <div class="mapouter">
                         <div class="gmap_canvas">
-                            <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=Brogatan 5, 903 21 Umeå&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                            <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=Brogatan+5,+903+21+Umeå&t=&z=13&ie=UTF8&iwloc=&output=embed">
                             </iframe>
                         </div>
                         <a href="https://www.crocothemes.net"></a>
                         <style>.mapouter{overflow:hidden;}.gmap_canvas {background:none!important;}</style>
                     </div>
+                    <h3 class="mobile-contact-info-text">kgunillak@gmail.com</h3>
                 </div>
                 <div class="right-side">
                     <h1 class="fourth-page-title">Kontakt</h1>
--- js/script.js
+++ js/script.js
@@ -12,20 +12,22 @@ $(document).keydown(function (e) {
     }
   }
 });
-$(window).bind('mousewheel', function(event) {
-  if(!window.matchMedia("(max-width: 1000px)").matches) {
-    if (event.originalEvent.wheelDelta >= 0) {
-      console.log($(window).scrollTop());
-      if($(window).scrollTop() % screen.height < 10) {
-        $('html, body').stop();
-        $('html, body').animate({ scrollTop: $(window).scrollTop() - screen.height}, 500);
-      }
-    } else {
-      console.log($(window).scrollTop());
-      if($(window).scrollTop() % screen.height < 10) {
-        $('html, body').stop();
-        $('html, body').animate({ scrollTop:$(window).scrollTop() + screen.height}, 500);
-      }
-    }
+
+
+function toggleHamburger() {
+  if(document.getElementById("hamburger-menu").classList.contains("active-menu")) {
+    document.getElementById("hamburger-menu").classList.remove("active-menu");
+  } else {
+    document.getElementById("hamburger-menu").classList.add("active-menu");
   }
-});
+
+  if(document.getElementById("toggle-hamburger").classList.contains("inactive")) {
+    document.getElementById("toggle-hamburger").classList.remove("inactive");
+    document.getElementById("logo-img").classList.remove("inactive");
+    document.getElementById("body").classList.remove("noscroll");
+  } else {
+    document.getElementById("toggle-hamburger").classList.add("inactive");
+    document.getElementById("logo-img").classList.add("inactive");
+    document.getElementById("body").classList.add("noscroll");
+  }
+}
